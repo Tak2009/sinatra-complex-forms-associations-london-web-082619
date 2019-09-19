19b0bbdd0230ba38dc593765e2ab4245a67baf62
github username <github email address>
2019-09-19 15:25:09 +0100

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -4,13 +4,20 @@ class OwnersController < ApplicationController
     @owners = Owner.all
     erb :'/owners/index' 
   end
-
+  
+  # New
   get '/owners/new' do 
+    @pets = Pet.all # for owner to select his/her(has many) pets when owner instance created
     erb :'/owners/new'
   end
 
-  post '/owners' do 
-    
+  # Create recieve data from New
+  post '/owners' do
+    @owner = Owner.create(params[:owner])
+        if !params[:pet][:name].empty?
+          @owner.pets << Pet.create(name: params[:pet][:name])
+        end
+    redirect to "/owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
@@ -18,12 +25,24 @@ class OwnersController < ApplicationController
     erb :'/owners/edit'
   end
 
+  # Show
   get '/owners/:id' do 
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
   end
 
   patch '/owners/:id' do 
-   
+   ####### bug fix
+   if !params[:owner].keys.include?(:pet_ids)
+    params[:owner][:pet_ids] = []
+    end
+    #######
+ 
+    @owner = Owner.find(params[:id])
+    @owner.update(params[:owner])
+    if !params[:pet][:name].empty?
+      @owner.pets << Pet.create(name: params[:pet][:name])
+    end
+    redirect "owners/#{@owner.id}"
   end
 end
